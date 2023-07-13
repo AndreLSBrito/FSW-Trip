f0d7f90b6f1701d6a23aa93ede7f08dad17d39e7
Tiotedd <andrelsbrito2022@gmail.com>
2023-07-13 20:57:30 -0300

    style: adpt trip reservation to desktop

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -22,18 +22,25 @@ const TripDetails = async ({params}: {params: {tripId:string}}) => {
   if(!trip) return null
 
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto lg:px-40">
       <TripHeader trip={trip}/>
 
-      <TripReservation 
-        maxGuests={trip.maxGuests}  
-        pricePerDay={trip.pricePerDay as any} 
-        tripEndDate={trip.endDate} 
-        tripId={trip.id}
-        tripStartDate={trip.startDate}  
-      />
-      <TripDescription description={trip.description}/>
-      <TripHightlights hightlights={trip.highlights}/>
+      <div className="flex flex-col lg:flex-row lg:mt-12 lg:gap-20">
+        <div className="lg:order-2">
+          <TripReservation 
+            maxGuests={trip.maxGuests}  
+            pricePerDay={trip.pricePerDay as any} 
+            tripEndDate={trip.endDate} 
+            tripId={trip.id}
+            tripStartDate={trip.startDate}  
+          />
+        </div>
+
+        <div className="lg:order-1">
+          <TripDescription description={trip.description}/>
+          <TripHightlights hightlights={trip.highlights}/>
+        </div>
+      </div>
       <TripLocation location={trip.location} locationDescription={trip.locationDescription}/>
     </div>
   )
