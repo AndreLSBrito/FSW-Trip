a2b3fd5947abafe64399cd3e98a4dd0b344fa1af
Tiotedd <andrelsbrito2022@gmail.com>
2023-07-13 21:51:09 -0300

    fix: adjust logo src

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Footer() {
   return ( 
     <div className="bg-walterWhite flex flex-col p-5 justify-center items-center">
-      <Image src="/logo.png" width={133} height={23} alt='Full Stack week'/>
+      <Image src="/Logo.png" width={133} height={23} alt='Full Stack week'/>
       <p className="text-sm font-medium mt-1 text-primaryDarker">Todos os direitos reservados.</p>
     </div>
    );
