6d690647e47c84b8da872df99235d92b9cb8ff4b
Tiotedd <andrelsbrito2022@gmail.com>
2023-07-13 21:11:24 -0300

    style: adapt details trip to desktop

--- src/app/trips/[tripId]/components/TripHighlights.tsx
+++ src/app/trips/[tripId]/components/TripHighlights.tsx
@@ -7,14 +7,15 @@ interface HightlightsDescriptionProps {
 
 const TripHightlights = ({hightlights}: HightlightsDescriptionProps) => {
   return (
-    <div className="flex flex-col p-5">
-      <h2 className='font-semibold text-primaryDarker mb-2'>Destaques</h2>
-      <div className="flex flex-wrap gap-y-3">
+    <div className="flex flex-col p-5 lg:p-o lg:mt-12">
+      <h2 className='font-semibold text-primaryDarker mb-2 lg:text-xl'>Destaques</h2>
+
+      <div className="flex flex-wrap gap-y-3 lg:mt-5">
         {hightlights.map((hightlights, index) => (
-          <div key={hightlights} className="flex item-center gap-2 w-1/2">
+          <div key={hightlights} className="flex item-center gap-2 lg:gap-3 w-1/2">
             <Image src="/check-icon.png" width={15} height={15} alt={hightlights}/>
 
-            <p className='text-grayPrimary text-xs '>{hightlights}</p>
+            <p className='text-grayPrimary text-xs lg:text-base'>{hightlights}</p>
           </div>
         ))}
       </div>
