242efce1d1404c88b9961a9ca7667bb3e28873e8
Tiotedd <andrelsbrito2022@gmail.com>
2023-07-20 12:38:36 -0300

    style: add animation spin on loading component

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Loading = () => {
-  return <div>Carregando...</div>
+  return (
+    <div className='container mx-auto flex mt-40 flex-col items-center justify-center text-primaryDarker'>
+      <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+        <circle cx="50" cy="50" r="40" className='bg-primary' stroke-width="10" />
+      </svg>
+      carregando...
+  </div>
+  )
 }
 
 export default Loading
